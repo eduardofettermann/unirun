b0ef8fede4ac1566cd9a2dff2ad6127e636175c0
Eduardo Fettermann <eduardofettermann212@gmail.com>
2025-11-24 03:31:17 -0300

    feat: adiciona model de user

---
+++ src/main/java/br/unisinos/unirun/user/model/User.java
@@ -0,0 +1,47 @@
+package br.unisinos.unirun.user.model;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "app_user")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @Column(name = "user_type")
+    private String userType;
+
+    @Column(name = "domain_id")
+    private Long domainId;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUserType() {
+        return userType;
+    }
+
+    public void setUserType(String userType) {
+        this.userType = userType;
+    }
+
+    public Long getDomainId() {
+        return domainId;
+    }
+
+    public void setDomainId(Long domainId) {
+        this.domainId = domainId;
+    }
+}
